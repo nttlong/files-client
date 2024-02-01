b37bf1dd7126ac2ffda3e293f72d08b4700d8dd7
admin <zugeliang2000@gmail.com>
2024-02-01 10:15:35 +0700

    Delete unuse Project

--- DesktopConnector/Libs/UIImplements/obj/Debug/net7.0-maccatalyst/UIImplements.AssemblyInfo.cs
+++ DesktopConnector/Libs/UIImplements/obj/Debug/net7.0-maccatalyst/UIImplements.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UIImplements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039f7f9f76944aab7287b79f501224dd85f79b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0115eb045869ceda6d9a23ee91a27ec4fd395cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UIImplements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UIImplements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
